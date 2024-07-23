fe7b2674f7c05af396062671848a0acd34fff7ee
jerryyehself <jerry40522@gmail.com>
2024-07-23 16:25:08 +0800

    位置相加為目標

--- 1.two-sum.php
+++ 1.two-sum.php
@@ -1,3 +1,4 @@
+<?php
 /*
  * @lc app=leetcode id=1 lang=php
  *
@@ -5,16 +6,23 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 
     /**
      * @param Integer[] $nums
      * @param Integer $target
      * @return Integer[]
      */
-    function twoSum($nums, $target) {
-        
+    function twoSum($nums, $target)
+    {
+
+        foreach ($nums as $key => $val) {
+            foreach ($nums  as $key2 => $val2) {
+                if (($key != $key2) && ($val + $val2 == $target))
+                    return [$key, $key2];
+            }
+        }
     }
 }
 // @lc code=end
-
